4391a71b51526226103546ed38b6d926d018b5ca
BoykoAA <BoykoA.A@yandex.ru>
2018-02-12 19:38:29 +0300

    add gcd

---
+++ gcd/gcd.py
@@ -0,0 +1,16 @@
+def gcd(a, b):
+    if a == 0 or b == 0:
+        return max(a, b)
+    else:
+        if a > b:
+            return gcd(a % b, b)
+        else:
+            return gcd(a, b % a)
+
+def main():
+    a, b = map(int, input().split())
+    print(gcd(a, b))
+
+
+if __name__ == "__main__":
+    main()
