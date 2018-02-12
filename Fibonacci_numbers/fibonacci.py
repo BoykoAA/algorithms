fb7d28cf70c67ac42f8efc23d2355b53734c2de0
BoykoAA <BoykoA.A@yandex.ru>
2018-02-12 16:28:31 +0300

    add Fibonacci_numbers

---
+++ Fibonacci_numbers/fibonacci.py
@@ -0,0 +1,16 @@
+def fib(n):
+    F = list(range(0, n+2))
+    F[0] = 0
+    F[1] = 1
+    if n == 1:
+        return (n)
+    for i in list(range(2, F[n+1])):
+        F[i] = F[i-1] + F[i-2]
+    return F[i]
+def main():
+    n = int(input())
+    print(fib(n))
+
+
+if __name__ == "__main__":
+    main()
