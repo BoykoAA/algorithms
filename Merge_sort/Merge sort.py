15105a8bc839c4d33fe7e3f0d7e273716c7fb96a
BoykoAA <BoykoA.A@yandex.ru>
2018-03-22 20:44:44 +0300

    add all

--- Merge_sort/Merge sort.py
+++ Merge_sort/Merge sort.py
@@ -28,3 +28,32 @@ def main():
 if __name__ == '__main__':
     k = 0
     main()
+
+# Второй вариант
+    # def merg_sort(A):
+    #
+    #     if len(A) <= 1:
+    #         return A
+    #
+    #     middle = int(len(A) / 2)
+    #     left = merg_sort(A[:middle])
+    #     right = merg_sort(A[middle:])
+    #
+    #     return merg(left, right)
+    #
+    #
+    # def merg(left, right):
+    #     result = []
+    #
+    #     while len(left) > 0 and len(right) > 0:
+    #         if left[0] <= right[0]:
+    #             result.append(left[0])
+    #             left = left[1:]
+    #         else:
+    #             result.append(right[0])
+    #             right = right[1:]
+    #     if len(left) > 0:
+    #         result += left
+    #     if len(right) > 0:
+    #         result += right
+    #     return result
